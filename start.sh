c6bf580c4965b8b5b22d17fe4156d8b554985ca5
mywebrai <raigit@atomicmail.io>
2026-08-31 13:55:02 +0330

    fix 3
    Changed shebang from bash to sh and updated comments.

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 export PORT=${PORT:-3000}
 
-# اجرای 3x-ui در پس‌زمینه
+# اجرای 3x-ui در پس‌زمینه (خودش Xray رو اجرا می‌کنه)
 /usr/local/x-ui/x-ui > /var/log/x-ui.log 2>&1 &
 
 # تنظیم پورت در nginx
