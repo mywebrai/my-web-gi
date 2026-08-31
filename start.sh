78716874f9178633d56bb066e72f9178c98ad65c
mywebrai <raigit@atomicmail.io>
2026-08-31 13:49:18 +0330

    fix 2
    Removed redundant Xray start command and updated comments.

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 set -e
 
-# تنظیم پورت
 export PORT=${PORT:-3000}
 
-# شروع Nginx با قالب
+# اجرای 3x-ui در پس‌زمینه
+/usr/local/x-ui/x-ui > /var/log/x-ui.log 2>&1 &
+
+# تنظیم پورت در nginx
 envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# شروع Xray
-nohup /usr/local/x-ui/x-ui > /var/log/x-ui.log 2>&1 &
-
-# شروع Nginx
+# اجرای nginx
 nginx -g "daemon off;"
