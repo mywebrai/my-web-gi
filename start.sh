05634f44bbe2f3fd15606d9515c5899239ab82b2
mywebrai <raigit@atomicmail.io>
2026-09-01 11:49:59 +0330

    there
    Removed unnecessary commands and set x-ui to execute directly.

--- start.sh
+++ start.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-set -e
-
-export PORT=${PORT:-3000}
-
-# اجرای 3x-ui در پس‌زمینه (خودش Xray رو اجرا می‌کنه)
-/usr/local/x-ui/x-ui > /var/log/x-ui.log 2>&1 &
-
-# تنظیم پورت در nginx
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# اجرای nginx
-nginx -g "daemon off;"
+exec /usr/local/x-ui/x-ui
